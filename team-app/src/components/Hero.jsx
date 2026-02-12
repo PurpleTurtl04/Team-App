4137e794c016801cb41dcd4f3b96861dcfab308b
Nick Edwards <nledwa01@gmail.com>
2026-02-12 11:38:03 -0600

    Added Hero content. Adjusted mobile spacing and alignment.

--- team-app/src/components/Hero.jsx
+++ team-app/src/components/Hero.jsx
@@ -3,8 +3,29 @@ import Navbar from './Navbar';
 function Hero() {
     return (
         <section>
-            <div className="bg-shift-left-mobile container mx-auto h-screen max-h-270 max-w-480 bg-[url('assets/team-app-hero-img.jpg')] bg-cover bg-center">
+            <div className="bg-shift-left-mobile h-screen max-h-270 max-w-480 bg-[url('assets/team-app-hero-img.jpg')] bg-cover bg-center">
                 <Navbar />
+                <div className='container mx-auto mt-40 flex flex-col justify-center px-8 text-white md:px-0'>
+                    <h1 className='mb-4 text-4xl'>
+                        Instant collaboration for remote teams
+                    </h1>
+                    <h4 className='mb-12 max-w-sm'>
+                        All-in-one place for your remote team to chat,
+                        collaborate and track project progress.
+                    </h4>
+                    <div className='flex flex-col gap-2'>
+                        <input
+                            type='email'
+                            id='email-address'
+                            class='max-w-xs rounded-sm border border-gray-300 bg-gray-50 p-5 text-sm text-gray-900'
+                            placeholder='name@example.com'
+                            required
+                        />
+                        <button className='bg-teamapp-blue hover:bg-teamapp-blue/80 max-w-38 rounded-sm p-5 transition duration-150 ease-in-out'>
+                            Get Early Access
+                        </button>
+                    </div>
+                </div>
             </div>
         </section>
     );
--- team-app/src/components/Navbar.jsx
+++ team-app/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import HamburgerMenu from './HamburgerMenu';
 function Navbar() {
     return (
         <div>
-            <div className='container mx-auto flex items-center justify-between pt-10 pb-15 md:pb-36 xl:px-10'>
+            <div className='3xl:px-10 container mx-auto flex items-center justify-between pt-10 pb-15 md:pb-36'>
                 <p className='font-poppins pl-3 text-3xl md:pl-0 lg:pb-2'>
                     <span className='font-bold text-white'>team</span>
                     <span className='text-teamapp-blue font-bold'>.</span>
