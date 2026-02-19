b58e161f61f5d1b319aff4f5473a4f824ce3e7c3
Nick Edwards <nledwa01@gmail.com>
2026-02-19 15:43:39 -0600

    Updated reviews section and included profile photos

--- team-app/src/components/Reviews.jsx
+++ team-app/src/components/Reviews.jsx
@@ -1,5 +1,70 @@
+import amyGoldberg from '../assets/AmyGoldberg.png';
+import linaSloan from '../assets/LinaSloan.png';
+import jonJandali from '../assets/JonJandali.png';
+import stars from '../assets/stars.svg';
+
+const reviewsData = [
+    {
+        name: 'Amy Goldberg',
+        title: 'COO at Slack',
+        comment:
+            'Faucibus. At penatibus ad cubilia risus senectus varius sociis suspendisse integer suspendisse turpis senectus pellentesque nibh iaculis.',
+        image: amyGoldberg,
+        grid: 'col-span-1 md:col-span-3 lg:col-span-1',
+    },
+    {
+        name: 'Lina Sloan',
+        title: 'Content Writer at Uber',
+        comment:
+            'Eros nulla mollis nec tortor. Eu class ante accumsan habitant morbi neque hac purus magnis varius pulvinar hac pede magnis.',
+        image: linaSloan,
+        grid: 'col-span-1 md:col-span-3 md:col-start-4 lg:col-span-1',
+    },
+    {
+        name: 'Jon Jandali',
+        title: 'CTO at Loom',
+        comment:
+            'Sociosqu habitasse vulputate odio nisi phasellus. Cras luctus sit leo pharetra nec semper commodo rutrum aptent feugiat.',
+        image: jonJandali,
+        grid: 'col-span-1 md:col-span-4 md:col-start-2 lg:col-span-1 ml-0 md:ml-12 lg:ml-0',
+    },
+];
+// mb-10 mb-15
 function Reviews() {
-    return <div></div>;
+    return (
+        <div className='container mx-auto mb-40 flex flex-col items-center px-8 lg:items-start'>
+            <h2 className='text-teamapp-darkblue mb-5 max-w-xs text-center text-4xl md:mb-10 md:max-w-md md:text-left lg:mb-15 lg:max-w-none lg:text-5xl'>
+                What people say about{' '}
+                <span className='text-teamapp-blue font-semibold'>Team</span>
+            </h2>
+            <div className='grid grid-cols-1 gap-4 md:grid-cols-6 lg:grid-cols-3'>
+                {reviewsData.map((review, index) => (
+                    <div
+                        key={index}
+                        className={`text-teamapp-grey flex max-w-sm flex-col border border-gray-200 bg-white p-6 drop-shadow-md ${review.grid}`}
+                    >
+                        <img
+                            src={stars}
+                            alt='golden stars'
+                            className='mt-10 mb-6 w-25'
+                        />
+                        <p>{review.comment}</p>
+                        <div className='mt-20 mb-6 flex gap-5'>
+                            <img
+                                src={review.image}
+                                alt={`Image of ${review.name}`}
+                                className='h-12 w-12'
+                            />
+                            <div className='flex flex-col'>
+                                <p className='font-bold'>{review.name}</p>
+                                <p>{review.title}</p>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default Reviews;
