0a843d06344d8a4f36235719516e6dc7f94688c6
Nick Edwards <nledwa01@gmail.com>
2026-02-19 11:24:12 -0600

    Removing embla carousel and cleaning up code

--- team-app/src/components/Reviews.jsx
+++ team-app/src/components/Reviews.jsx
@@ -1,21 +1,5 @@
-import useEmblaCarousel from 'embla-carousel-react';
-
 function Reviews() {
-    const [emblaRef] = useEmblaCarousel();
-
-    return (
-        <div className='embla'>
-            <div className='embla__viewport' ref={emblaRef}>
-                <div className='embla__container'>
-                    <div className='embla__slide'>Slide 1</div>
-                    <div className='embla__slide'>Slide 2</div>
-                    <div className='embla__slide'>Slide 3</div>
-                </div>
-            </div>
-
-            {/* Optional: Add navigation controls here */}
-        </div>
-    );
+    return <div></div>;
 }
 
 export default Reviews;
