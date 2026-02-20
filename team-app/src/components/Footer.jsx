6a63f48dd4d96eb5e140f6c4decad4edd63a56e1
Nick Edwards <nledwa01@gmail.com>
2026-02-20 08:30:51 -0600

    Added arrow submit button inside the footer input field. Cleaned up old code

--- team-app/src/components/Footer.jsx
+++ team-app/src/components/Footer.jsx
@@ -33,19 +33,16 @@ function Footer() {
                 <div className='grid grid-cols-1 gap-4 sm:grid-cols-4 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-8'>
                     {/* Primary Column (e.g., Logo, Description) */}
                     <div className='col-span-1 mb-4 text-center sm:col-span-4 md:col-span-3 lg:col-span-4 lg:text-left xl:col-span-2'>
-                        <h3 className='font-poppins text-3xl lg:pb-2'>
+                        <h3 className='font-poppins text-3xl xl:pb-2'>
                             <span className='font-bold text-white'>team</span>
                             <span className='text-teamapp-blue font-bold'>
                                 .
                             </span>
                         </h3>
-                        <p className='text-gray-400 lg:max-w-40'>
+                        <p className='text-gray-400 xl:max-w-40'>
                             Collaboration platform for modern teams.
                         </p>
                     </div>
-
-                    {/* Four Evenly Distributed Columns */}
-                    {/* <div className='grid-cols-1'> */}
                     {footerData.map((section, index) => (
                         <div
                             key={index}
@@ -77,20 +74,38 @@ function Footer() {
                         </div>
                     ))}
                     <div className='col-span-1 mb-6 flex flex-col items-center text-center sm:col-span-2 md:col-span-3 md:mt-10 md:mb-0 md:text-left lg:col-span-1 lg:mt-0 lg:items-start lg:text-left xl:col-start-7'>
-                        <h4 className='mb-2 text-center text-[18px] lg:mb-4 xl:text-left'>
+                        <h4 className='mb-2 text-center text-[18px] xl:text-left'>
                             Stay up to date
                         </h4>
-                        <p className='text-teamapp-white/40 text-[14px]'>
+                        <p className='text-teamapp-white/40 min-w-60 text-center text-[14px] lg:text-left'>
                             Subscribe to our newsletter
                         </p>
-                        <input
-                            type='email'
-                            id='email-address'
-                            className='text-teamapp-grey w-64 rounded-sm bg-white/10 p-5 text-sm shadow-lg'
-                            placeholder='name@example.com'
-                        />
+                        <div className='relative'>
+                            <input
+                                type='email'
+                                id='email-address'
+                                className='relative w-64 rounded-sm bg-white/10 p-5 text-sm text-white/80 shadow-lg'
+                                placeholder='name@example.com'
+                            />
+                            <button
+                                type='submit'
+                                class='hover:text-teamapp-yellow absolute inset-y-0 right-0 flex items-center pr-3 text-white/60'
+                            >
+                                <svg
+                                    xmlns='http://www.w3.org/2000/svg'
+                                    class='h-5 w-5'
+                                    viewBox='0 0 20 20'
+                                    fill='currentColor'
+                                >
+                                    <path
+                                        fill-rule='evenodd'
+                                        d='M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z'
+                                        clip-rule='evenodd'
+                                    />
+                                </svg>
+                            </button>
+                        </div>
                     </div>
-                    {/* </div> */}
                 </div>
 
                 {/* Copyright Section */}
@@ -106,79 +121,3 @@ function Footer() {
 }
 
 export default Footer;
-
-// <div className='bg-teamapp-footerblue pt-15 text-white'>
-//     <div className='container mx-auto max-w-7xl px-6 py-10'>
-//         {/* Main Footer Section (Primary column + Four small columns) */}
-//         <div className='flex flex-wrap justify-between gap-4'>
-//             {/* Primary Column (e.g., Logo, Description) */}
-//             <div className='w-full text-center md:w-1/4 md:text-left'>
-//                 <h3 className='font-poppins text-3xl lg:pb-2'>
-//                     <span className='font-bold text-white'>team</span>
-//                     <span className='text-teamapp-blue font-bold'>
-//                         .
-//                     </span>
-//                 </h3>
-//                 <p className='text-gray-400 lg:max-w-40'>
-//                     Collaboration platform for modern teams.
-//                 </p>
-//             </div>
-
-//             {/* Four Evenly Distributed Columns */}
-//             <div className='flex w-full flex-wrap justify-between md:w-2/3'>
-//                 {footerData.map((section, index) => (
-//                     <div
-//                         key={index}
-//                         className='mb-6 flex w-1/2 flex-col items-center text-center md:mb-0 md:w-1/4 md:items-start md:text-left'
-//                     >
-//                         <h4 className='mb-4 text-left text-lg'>
-//                             {section.title}
-//                         </h4>
-//                         <ul className='flex flex-col gap-3'>
-//                             {section.links.length > 0
-//                                 ? section.links.map((link) => (
-//                                       <li key={link}>
-//                                           <a
-//                                               href=''
-//                                               className='text-teamapp-white/40 hover:text-teamapp-yellow transition duration-200 ease-in-out'
-//                                           >
-//                                               {link}
-//                                           </a>
-//                                       </li>
-//                                   ))
-//                                 : section.contacts.map((contact) => (
-//                                       <li key={contact}>
-//                                           <p className='text-teamapp-white/40 hover:text-teamapp-yellow transition duration-200 ease-in-out'>
-//                                               {contact}
-//                                           </p>
-//                                       </li>
-//                                   ))}
-//                         </ul>
-//                     </div>
-//                 ))}
-//                 <div className='mb-6 flex w-1/2 flex-col items-center text-center md:mb-0 md:w-1/4 md:items-start md:text-left'>
-//                     <h4 className='mb-4 text-left text-lg'>
-//                         Stay up to date
-//                     </h4>
-//                     <p className='text-teamapp-white/40'>
-//                         Subscribe to our newsletter
-//                     </p>
-//                     <input
-//                         type='email'
-//                         id='email-address'
-//                         className='w-xs rounded-sm border border-gray-300 bg-gray-50 p-5 text-sm text-gray-900 shadow-lg'
-//                         placeholder='name@example.com'
-//                     />
-//                 </div>
-//             </div>
-//         </div>
-
-//         {/* Copyright Section */}
-//         <div className='mt-20 text-center md:mt-40 md:text-left'>
-//             <p className='text-teamapp-white/40'>
-//                 &copy; {new Date().getFullYear()} Copyright Team Inc.
-//                 All rights reserved.
-//             </p>
-//         </div>
-//     </div>
-// </div>;
